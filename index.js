dcd42163f579c04daad78de06cb786c7922db8a4
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-19 15:35:04 +0530

    change3

--- index.js
+++ index.js
@@ -24,17 +24,32 @@
 
 
 // <--------------------------------------------5th QUESTION-------------------------------------------->
-let a = parseInt(process.argv[2])
-let b = parseInt(process.argv[3])
-let product = 1;
-for(let i = a;i<=b ;i ++){
-  let checking = Math.floor(i/10);
-     if(i%2==0 && checking%10==4){
-    product = product * i;
-    }
-}
-if(product ==1 ){
- console.log(0);
-}else{
-    console.log(product);
-  }
+
+
+// let a = parseInt(process.argv[2])
+// let b = parseInt(process.argv[3])
+// let product = 1;
+// for(let i = a;i<=b ;i ++){
+//   let checking = Math.floor(i/10);
+//      if(i%2==0 && checking%10==4){
+//     product = product * i;
+//     }
+// }
+// if(product ==1 ){
+//  console.log(0);
+// }else{
+//     console.log(product);
+//   }
+
+// <--------------------------------------------4th QUESTION-------------------------------------------->
+
+let num = 153;
+ let firstDigit = Math.floor(num/100);
+ let secondDigit = Math.floor((num%100)/10);
+ let lastDigit = ((num%100)%10);
+let armstrong = firstDigit**3 + secondDigit**3 + lastDigit**3 
+ if( armstrong == num){
+  console.log("Its an armstrong number");
+ }else{
+  console.log("Its not a  armstrong number");
+ }
