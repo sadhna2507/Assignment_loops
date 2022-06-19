b25cae69ae090c931509d93aef4f18f887e7c577
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-19 14:40:59 +0530

    change2

--- index.js
+++ index.js
@@ -9,13 +9,32 @@
 //   }
 
 
-// <--------------------------------------------1st QUESTION-------------------------------------------->
+// <--------------------------------------------2nd QUESTION-------------------------------------------->
+
+
+// let a = parseInt(process.argv[2])
+// let b = parseInt(process.argv[3])
+// let sum=0;
+// for(i=a;i<=b;i++){
+//   if(i%2==0){
+//     sum+=i;
+//   }
+// }
+// console.log(sum);
+
+
+// <--------------------------------------------5th QUESTION-------------------------------------------->
 let a = parseInt(process.argv[2])
 let b = parseInt(process.argv[3])
-let sum=0;
-for(i=a;i<=b;i++){
-  if(i%2==0){
-    sum+=i;
-  }
+let product = 1;
+for(let i = a;i<=b ;i ++){
+  let checking = Math.floor(i/10);
+     if(i%2==0 && checking%10==4){
+    product = product * i;
+    }
 }
-console.log(sum);
+if(product ==1 ){
+ console.log(0);
+}else{
+    console.log(product);
+  }
