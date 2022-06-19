ec471547bc4d3a962be44b836668236499bd0b19
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-19 12:58:42 +0530

    change1

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // <--------------------------------------------1st QUESTION-------------------------------------------->
-  let n = parseInt(process.argv[2])
-   for(let i=0+1; i<n; i++){
-    if((i%2)!==0){
-      console.log(i);
-    }
+
+
+//   let n = parseInt(process.argv[2])
+//    for(let i=0+1; i<n; i++){
+//     if((i%2)!==0){
+//       console.log(i);
+//     }
+//   }
+
+
+// <--------------------------------------------1st QUESTION-------------------------------------------->
+let a = parseInt(process.argv[2])
+let b = parseInt(process.argv[3])
+let sum=0;
+for(i=a;i<=b;i++){
+  if(i%2==0){
+    sum+=i;
   }
+}
+console.log(sum);
