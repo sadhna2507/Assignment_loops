02d7cda1339b91286b314e8af23080c97680e97d
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-19 16:51:04 +0530

    change4

--- index.js
+++ index.js
@@ -43,13 +43,53 @@
 
 // <--------------------------------------------4th QUESTION-------------------------------------------->
 
-let num = 153;
- let firstDigit = Math.floor(num/100);
- let secondDigit = Math.floor((num%100)/10);
- let lastDigit = ((num%100)%10);
-let armstrong = firstDigit**3 + secondDigit**3 + lastDigit**3 
- if( armstrong == num){
-  console.log("Its an armstrong number");
- }else{
-  console.log("Its not a  armstrong number");
- }
+
+// let num = 153;
+//  let firstDigit = Math.floor(num/100);
+//  let secondDigit = Math.floor((num%100)/10);
+//  let lastDigit = ((num%100)%10);
+// let armstrong = firstDigit**3 + secondDigit**3 + lastDigit**3 
+//  if( armstrong == num){
+//   console.log("Its an armstrong number");
+//  }else{
+//   console.log("Its not a  armstrong number");
+//  }
+
+
+// <--------------------------------------------4th QUESTION-------------------------------------------->
+
+
+// function even_digits(num) {
+//     let ctr = 0;
+//     while (num) {
+//       ctr += num % 2 === 0;
+//       num = Math.floor(num / 10);
+//     }
+//     return ctr;
+//   }
+  
+//   console.log(even_digits(123));
+//   console.log(even_digits(1020));
+//   console.log(even_digits(102));
+
+
+// <--------------------------------------------7th QUESTION-------------------------------------------->
+
+let rev = 0;
+let num = 167;
+let lastDigit;
+
+
+while(num != 0){
+	lastDigit = num % 10;
+  rev = rev * 10 + lastDigit;
+  num = Math.floor(num/10);
+  
+}
+if(rev===num){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
+console.log("Reverse number : "+rev);
